e134ebfe93844fd94f972f74b6d6b2b358334602
Aaron Klotnia <aklotnia@gmail.com>
2019-10-03 19:12:37 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,6 @@
 # Your code here
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  final_sentence= puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
