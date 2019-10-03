bcfe198e6219248042d938fc802adc5b87fce55d
Aaron Klotnia <aklotnia@gmail.com>
2019-10-03 19:13:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,6 +2,5 @@
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  final_sentence = "A plate of #{protein} with #{veg1} and #{veg2}."
-  return final_sentence
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
